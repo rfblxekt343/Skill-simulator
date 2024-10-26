5105a280f4a067c478af1ad50dc8dc736bd61383
nativlevy <nlev245@gmail.com>
2024-10-26 17:52:31 +0300

    asdasd

--- components/LogInForm.jsx
+++ components/LogInForm.jsx
@@ -83,7 +83,7 @@ export default function LogInForm() {
        <div
         className="container mx-auto p-6 max-w-lg card bg-white shadow-lg rounded-lg"
         style={{
-          backgroundImage: "url('/formBackground.jpפ')",
+          backgroundImage: "url('/formBackground.jpg')",
           backgroundSize: "contain", 
           backgroundSize: "cover", // Ensure the background covers the whole container
           backgroundRepeat: "no-repeat", // Prevent the background from repeating
